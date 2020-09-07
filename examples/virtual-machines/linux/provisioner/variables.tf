010d255f6a730a030d0362e5c81b8f8831dd198a
sindhuja <sindhuja@appranix.com>
2020-09-07 17:21:33 +0530

    Sample test

--- examples/virtual-machines/linux/provisioner/variables.tf
+++ examples/virtual-machines/linux/provisioner/variables.tf
@@ -1,7 +1,10 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  default = "sindhuja"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default = "East US"
 }
+
